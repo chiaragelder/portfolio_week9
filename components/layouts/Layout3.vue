aa7219f6994814321db444b1699daf93ecf7bd75
“Chiara <chiaragelder@gmail.com>
2025-01-17 15:33:32 +0100

    interlinie layout3 titel

--- components/layouts/Layout3.vue
+++ components/layouts/Layout3.vue
@@ -23,7 +23,7 @@
         >
           <div class="p-4 text-center">
             <!-- Responsieve tekst -->
-            <h1 class="text-white text-4xl md:text-6xl lg:text-8xl font-bold mb-6 title-line-height">{{ data.title }}</h1>
+            <h1 class="text-white text-4xl md:text-6xl lg:text-8xl font-bold mb-6 mijn-tekst">{{ data.title }}</h1>
             <h1 v-if="data.subtitle" class="text-white opacity-80 pt-3 text-lg md:text-2xl lg:text-3xl font-bold pb-10">{{ data.subtitle }}</h1>
             <p v-if="data.author" class="text-white opacity-80 text-sm md:text-base lg:text-lg font-bold mb-2">{{ data.author }}</p>
             <p class="text-white text-xs opacity-50 hover:opacity-100">Last update: {{ formatDate(data.date) }}</p>
@@ -41,7 +41,7 @@
         <div class="container grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
           <!-- First column -->
           <div>
-            <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold mb-4">{{ data.title }}</h1>
+            <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold mb-4 mijn-tekst">{{ data.title }}</h1>
             <h1 v-if="data.subtitle" class="opacity-80 text-lg md:text-xl lg:text-2xl font-bold pb-6">{{ data.subtitle }}</h1>
             <p class="text-base md:text-lg lg:text-xl pb-6 font-bold">{{ data.description }}</p>
           </div>
@@ -100,29 +100,28 @@ defineProps(['data', 'formatDate']);
   animation: spin 1s linear infinite;
 }
 
-/* Dynamische line-height voor de titel */
 .title-line-height {
-  line-height: 1.4; /* Standaard regelhoogte */
+  line-height: 1.6; /* Standaard regelhoogte verhoogd */
 }
 
-/* Media queries voor verschillende schermgroottes */
 @media (max-width: 768px) {
   .title-line-height {
-    line-height: 1.3; /* Kleinere interlinie voor mobiel */
+    line-height: 1.5; /* Grotere interlinie voor mobiel */
   }
 }
 
 @media (min-width: 768px) and (max-width: 1024px) {
   .title-line-height {
-    line-height: 1.4; /* Interlinie voor tablets en medium schermen */
+    line-height: 1.6; /* Grotere interlinie voor tablets en medium schermen */
   }
 }
 
 @media (min-width: 1024px) {
   .title-line-height {
-    line-height: 1.5; /* Grotere interlinie voor desktop schermen */
+    line-height: 1.7; /* Grotere interlinie voor desktop schermen */
   }
 }
+
 
 /* Keyframe Animatie */
 @keyframes spin {
@@ -133,4 +132,9 @@ defineProps(['data', 'formatDate']);
     transform: rotate(360deg);
   }
 }
+
+.mijn-tekst {
+  line-height: 1.3 ; /* Stelt de interlinie in op 1.8 keer de lettergrootte */
+}
+
 </style>
